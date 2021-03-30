79b1b74956c042f74f4b3216ff29e2c020605532
Michele Carmagnani <michele.carmagnani@gmail.com>
2021-03-30 15:36:30 +0200

    add action for user and longin

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,15 +1,40 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
+import ax from "axios";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    API: "http://localhost:3000",
   },
-  mutations: {
-  },
+  mutations: {},
   actions: {
+    async signup(ctx, data) {
+      let res = ax.post(`${ctx.state.API}/user`, {
+        username: data.username,
+        password: data.password,
+      });
+    },
+    async login(ctx, data) {
+      try {
+        let resp = await ax.post(
+          `${ctx.state.API}/auth/login`,
+          {
+            username: data.username,
+            password: data.password,
+          },
+          {
+            headers: {
+              Authorization: `Bearer ${sessionStorage.getItem("shuiToken")}`,
+            },
+          }
+        );
+        console.log("response", resp);
+      } catch (error) {
+        console.error(error);
+      }
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
