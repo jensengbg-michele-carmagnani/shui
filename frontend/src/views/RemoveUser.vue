dc634e8c08f892741dde88e48c8e70baba1eeb86
Michele Carmagnani <michele.carmagnani@gmail.com>
2021-04-04 09:21:36 +0200

    shadow root promlem in delete user

---
+++ frontend/src/views/RemoveUser.vue
@@ -0,0 +1,25 @@
+<template>
+  <select  id="remove-user">
+    <img class="goTo-login" @click="goToLogin" src="../assets/topS.png" alt="">
+    <p>you no longer exists</p>
+    <p>in Shui system</p>
+  </select>
+</template>
+
+<script>
+export default {
+  name: "RemoveUser",
+  data() {
+    return {
+      
+    }
+  },
+  methods: {
+    goToLogin(){
+      this.$router.push('/login')
+    }
+  },
+};
+</script>
+
+<style></style>
---
+++ frontend/src/views/UserDelete.vue
@@ -0,0 +1,25 @@
+<template>
+  <select  id="user-delete">
+    <img class="goTo-login" @click="goToLogin" src="../assets/topS.png" alt="">
+    <p>you no longer exists</p>
+    <p>in Shui system</p>
+  </select>
+</template>
+
+<script>
+export default {
+  name: "UserDelete",
+  data() {
+    return {
+      
+    }
+  },
+  methods: {
+    goToLogin(){
+      this.$router.push('/login')
+    }
+  },
+};
+</script>
+
+<style></style>
